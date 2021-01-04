4a99adf87b54c5a2839365b92fe21c13a91c053e
vivishu <kadiegui1999@gmail.com>
2021-01-04 12:28:19 +0000

    depot git

--- serveur.js
+++ serveur.js
@@ -1,7 +1,8 @@
 var express=require('express');
 var app=express();
 var port=8083;
-app.get('/',(req,res) => {
-    res.send('hello simplon ADEFNIFA')
-});
+app.set('view engine','ejs');
+app.get('/',(req,res)=>{
+    res.render('view')
+})
 app.listen(8083)
